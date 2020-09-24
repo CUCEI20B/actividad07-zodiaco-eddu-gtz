46c6b9dbfef833f4ffdf3ed010edcc25e0ed5a38
Eduardo Gutierrez <eduardo.gutierrez7981@alumnos.udg.mx>
2020-09-24 08:29:49 -0500

    Prueba02

--- main.py
+++ main.py
@@ -4,85 +4,85 @@ mes = int(input())
 #Enero
 if mes == 1:
     if dia >= 1 and dia <= 20:
-        print("Capricornio")
+        print("capricornio")
     elif dia >= 21 and dia <= 30:
-        print("Acuario")
+        print("acuario")
 
 #Febrero  
 elif mes == 2:
     if dia >= 1 and dia <= 19:
-        print("Acuario")
+        print("acuario")
     elif dia >= 20 and dia <= 28:
-        print("Picis")
+        print("picis")
 
 #Marzo   
 elif mes == 3:
     if dia >= 1 and dia <= 20:
-        print("Picis")
+        print("picis")
     elif dia >= 21 and dia <= 31:
-        print("Aries")
+        print("aries")
 
 #Abril   
 elif mes == 4:
     if dia >= 1 and dia <= 20:
-        print("Aries")
+        print("aries")
     elif dia >= 21 and dia <= 30:
-        print("Tauro")
+        print("tauro")
 
 #Mayo 
 elif mes == 5:
     if dia >= 1 and dia <= 21:
-        print("Tauro")
+        print("tauro")
     elif dia >= 22 and dia <= 31:
-        print("Géminis")
+        print("geminis")
 
 #Junio
 elif mes == 6:
     if dia >= 1 and dia <= 21:
-        print("Géminis")
+        print("geminis")
     elif dia >= 22 and dia <= 30:
-        print("Cáncer")
+        print("cancer")
 
 #Julio
 elif mes == 7:
     if dia >= 1 and dia <= 22:
-        print("Cáncer")
+        print("cancer")
     elif dia >= 23 and dia <= 31:
-        print("Leo")
+        print("leo")
 
 #Agosto   
 elif mes == 8:
     if dia >= 1 and dia <= 23:
-        print("Leo")
+        print("leo")
     elif dia >= 24 and dia <= 31:
-        print("Virgo")
+        print("virgo")
 
 #Septiembre   
 elif mes == 9:
     if dia >= 1 and dia <= 22:
-        print("Virgo")
+        print("virgo")
     elif dia >= 23 and dia <= 30:
-        print("Libra")
+        print("libra")
 
 #Octubre
 elif mes == 10:
     if dia >= 1 and dia <= 22:
-        print("Libra")
+        print("libra")
     elif dia >= 23 and dia <= 30:
-        print("Escorpion")
+        print("escorpion")
 
 #Noviembre       
 elif mes == 11:
     if dia >= 1 and dia <= 21:
-        print("Escorpion")
+        print("escorpion")
     elif dia >= 22 and dia <= 31:
-        print("Sagitario")
+        print("sagitario")
 
 #DICIEMBRE
 elif mes == 12:
     if dia >= 1 and dia <= 21:
-        print("Sagitario")
+        print("sagitario")
     elif dia >= 22 and dia <= 31:
-        print("Capricornio")
+        print("capricornio")
 else:
     print("No existe ese mes")
